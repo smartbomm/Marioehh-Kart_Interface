c1ef6c1f1f92d18c2efb32625bc2b78b0e49a793
j.bommeli <joel@bommeli.de>
2025-05-22 23:39:38 +0200

    Blub

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,103 @@
 //#include <samd21/include/samd21g18a.h>
 #include <Arduino.h>
-#include <BarcodeReader.h>
-#include <OdometerData.h>
+#include <Arduino_LSM6DS3.h>
+#include <MathFunctions.h>
 
-barcodeConfig_t config;
+uint32_t lastTime = 0;
 
-uint32_t speed;
-uint8_t barcodeValue;
-odometerData_t odometerData;
+struct accelerationData {
+    int16_t x;
+    int16_t y;
+    int16_t z;
+}raw, filtered, velocity, position;
+int32_t accel_xy = 0;
+int32_t velocity_xy = 0;
 
-void setup()
-{
-  config.pin = 2u;
-  config.bitLength = 7;
-  barcode_init(config);
-  Serial.begin(9600);
-  Serial.println("START");
+// Low-pass filter parameters
+moving_average_handler_t lowPassFilterX;
+moving_average_handler_t lowPassFilterY;
+moving_average_handler_t lowPassFilterZ;
+
+//Integration parameters
+integration_handler_t integrationX;
+integration_handler_t integrationY;
+integration_handler_t integrationZ;
+
+void setup() {
+    lowPassFilterX = moving_average_create(10);
+    lowPassFilterY = moving_average_create(10);
+    lowPassFilterZ = moving_average_create(10);
+
+    integrationX = integration_create();
+    integrationY = integration_create();
+    integrationZ = integration_create();
+
+    Serial.begin(9600);
+    while (!Serial) {
+        delay(10);
+    }
+    Serial.println("Hello, world!");
+    if (!IMU.begin()) {
+        Serial.println("Failed to initialize IMU!");
+        while (1);
+    }
+    while (!IMU.accelerationAvailable()) {
+        IMU.readAcceleration(raw.x, raw.y, raw.z);
+        raw.x = raw.x / 1000;
+        raw.y = raw.y / 1000;
+        raw.z = raw.z / 1000;
+        moving_average_init(lowPassFilterX, raw.x);
+        moving_average_init(lowPassFilterY, raw.y);
+        moving_average_init(lowPassFilterZ, raw.z);
+    }
+
+
+    uint32_t time = micros()/1000000;
+    integration_init(integrationX, raw.x, time);
+    integration_init(integrationY, raw.y, time);
+    integration_init(integrationZ, raw.z, time);
 }
+void loop() {
 
-void loop()
-{
-  if (READING_SUCCESSFUL == barcode_get(&barcodeValue, &speed))
-  {
-    char str[100];
-    sprintf(str, "Barcode read:\n----------------\nValue: %d\nSpeed [mm/s]: %d\n\n", barcodeValue, speed);
-    Serial.print(str);
-  }
+    
+    if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(raw.x, raw.y, raw.z);
+        raw.x = raw.x / 1000;
+        raw.y = raw.y / 1000;
+        raw.z = raw.z / 1000;
+    }
+    // Apply a low-pass filter to the raw acceleration data
+    moving_average_write(lowPassFilterX, raw.x);
+    moving_average_write(lowPassFilterY, raw.y);
+    moving_average_write(lowPassFilterZ, raw.z);
+    filtered.x = moving_average_read(lowPassFilterX);
+    filtered.y = moving_average_read(lowPassFilterY);
+    filtered.z = moving_average_read(lowPassFilterZ);
+    // Calculate velocity and position
+
+    uint32_t time = micros()/1000000;
+    integration_update(integrationX, filtered.x, time);
+    integration_update(integrationY, filtered.y, time);
+    integration_update(integrationZ, filtered.z, time);
+    velocity.x = integration_get_value(integrationX);
+    velocity.y = integration_get_value(integrationY);
+    velocity.z = integration_get_value(integrationZ);
+
+    // Print the results
+    if(time - lastTime > 1) {
+        Serial.print("Raw Acceleration: ");
+        Serial.print("X: "+String(raw.x));
+        Serial.print(" Y: "+String(raw.y));
+        Serial.print(" Z: "+String(raw.z));
+        Serial.println();
+        Serial.print("X: "+String(velocity.x));
+    Serial.print(" Y: "+String(velocity.y));
+    Serial.print(" Z: "+String(velocity.z));
+    Serial.println();
+
+        lastTime = time;
+    }
+    
+
+
 }
-
-/*int main() {
-
-  REG_GCLK_CLKCTRL |= 0b0100000000001001u;
-  REG_EIC_CTRL |= EIC_CTRL_ENABLE;  // Enable EIC
-  REG_EIC_INTENSET |= EIC_EVCTRL_EXTINTEO0;  // Enable event output on pin 0
-  REG_EIC_CONFIG0 |= EIC_CONFIG_SENSE0_BOTH;  // Set event on both edges
-  REG_PORT_DIR0 = 1<<17;
-  for(;;) {
-    REG_PORT_OUT0 = 1<<17;
-    for (int i = 0; i < 500000; i++) asm("nop");
-    REG_PORT_OUT0 = 0;
-    for (int i = 0; i < 500000; i++) asm("nop");
-
-  }
-
-
-  }*/
