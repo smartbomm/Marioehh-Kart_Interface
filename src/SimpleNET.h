16719b8893f1b2ecca5b1083a38f153c05c5778a
sgraml <gr.steff@gmx.de>
2025-06-24 10:37:42 +0200

    removed passwords

--- src/SimpleNET.h
+++ src/SimpleNET.h
@@ -35,35 +35,13 @@
 #define UDP_PORT 4210
 
 
-#ifdef DAHEIM
-#define SSID "WLAN"
-#define PASSWORD "06130705342415756002"
-#define SERVER_IP "192.168.178.42"
-#endif
-
-#ifdef HOFSPANNUNG
-#define SSID "HofSpannung"
-#define PASSWORD "8ZfjM4GHYVgrYa"
-#define SERVER_IP "192.168.1.62"
-#endif
-
 #ifdef LABOR
 #define SSID "Labore-Hof AT-MT"
 #define PASSWORD "laborwlan"
 #define SERVER_IP "192.168.9.150"
 #endif
 
-#ifdef HOF
-#define SSID "karmvc"
-#define PASSWORD "jvTtFfN3wC8g4UcQ"
-#define SERVER_IP "192.168.178.112"
-#endif
 
-#ifdef JOEL
-#define SSID "iPhone von Joel"
-#define PASSWORD "12345678"
-#define SERVER_IP "172.20.10.7"
-#endif
 
 
 
